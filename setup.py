8c2dc541d01c942af990f7a87f8261cf851a0951
Frederick Wagner <doctaphred@gmail.com>
2016-09-19 20:46:56 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name='emojencode',
+    version='1.0',
+    description='Encode binary data as emoji! 😄',
+    author='Frederick "doctaphred" Wagner',
+    author_email='yo@doctaph.red',
+    url='https://github.com/doctaphred/emojencode',
+    py_modules=['emojencode'],
+)
